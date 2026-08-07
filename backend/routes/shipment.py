c859a034f5af0341583edac61905c865ce5f01da
Suryaaa.. <jeevansurya560@gmail.com>
2026-08-07 18:32:43 +0530

    Enhance shipment API with database support
    Refactor shipment API to include database integration for fetching and creating shipments.

--- backend/routes/shipment.py
+++ backend/routes/shipment.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from models import Shipment
+from schemas import ShipmentCreate, ShipmentResponse
 
 router = APIRouter(
     prefix="/shipment",
     tags=["Shipment"]
 )
 
-@router.get("/")
-def get_shipments():
-    return {
-        "message": "Shipment API is working"
-    }
+@router.get("/", response_model=list[ShipmentResponse])
+def get_shipments(db: Session = Depends(get_db)):
+    return db.query(Shipment).all()
+
+
+@router.post("/", response_model=ShipmentResponse)
+def create_shipment(
+    shipment: ShipmentCreate,
+    db: Session = Depends(get_db)
+):
+    new_shipment = Shipment(**shipment.model_dump())
+    db.add(new_shipment)
+    db.commit()
+    db.refresh(new_shipment)
+    return new_shipment
